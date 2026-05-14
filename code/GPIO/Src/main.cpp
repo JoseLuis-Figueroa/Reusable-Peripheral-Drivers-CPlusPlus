c5561ecb4fc749ee182554691d50e9f525cb5d9a
Jose-Figueroa <figueroadelara@outlook.com>
2026-05-14 09:07:25 -0600

    Create GPIO.hpp header.
    Create Class Register.
    
    Create Class GPIODriver.

---
+++ code/GPIO/Src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 12, 2026
+ *      Author: figue
+ */
+#include "stm32f4xx.hpp"
+#include "GPIO.hpp"
+
+int main(void)
+{
+	//GPIODriver<0> GPIOA; // Create an instance of the GPIODriver for GPIOA
+
+	while (true)
+	{
+		//GPIOA.gpio_config(); // Initialize and configure the GPIOA peripheral
+	}
+
+
+}
